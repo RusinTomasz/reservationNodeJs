436a9358b7331d233f2eccc83b895b9c97c4fa08
RusinTomasz <tomaszrusin90@gmail.com>
2020-11-05 11:02:50 +0100

    Employee name in fetchClientAppoitments

--- src/controllers/client.js
+++ src/controllers/client.js
@@ -4,9 +4,18 @@ const createError = require("http-errors");
 exports.fetchClientAppoitments = async (req, res, next) => {
   try {
     const { clientId } = req.params;
+    let { archives } = req.query;
+
+    if (archives === "true") {
+      archives = true;
+    } else {
+      archives = false;
+    }
+
     const clientServiceInstance = new ClientService();
     const clientAppoitments = await clientServiceInstance.fetchClientAppoitments(
-      clientId
+      clientId,
+      archives
     );
     if (clientAppoitments instanceof Error) {
       throw createError(clientAppoitments.statusCode, clientAppoitments);
--- src/services/clientService.js
+++ src/services/clientService.js
@@ -1,17 +1,33 @@
 const Appoitment = require("../models/appointment");
-
+const { Op } = require("sequelize");
+const sequelize = require("sequelize");
+const Employee = require("../models/employee");
 class ClientService {
   constructor() {}
 
-  fetchClientAppoitments = async (clientId) => {
+  fetchClientAppoitments = async (clientId, archives) => {
+    const sequelizeOperator = archives ? Op.lt : Op.gte;
     const appoitments = await Appoitment.findAll({
       attributes: [
         "id",
         ["start_time", "startTime"],
         ["end_time_expected", "endTimeExpected"],
+        ["price_expected", "priceExpected"],
+      ],
+      include: [
+        {
+          model: Employee,
+          attributes: [
+            ["first_name", "firstName"],
+            ["last_name", "lastName"],
+          ],
+        },
       ],
       where: {
         clientId: clientId,
+        start_time: {
+          [sequelizeOperator]: sequelize.literal("CURRENT_TIMESTAMP"),
+        },
       },
       order: [["start_time", "ASC"]],
     })
