480eb97fac50ecf17130dbe7a4b5c1bb02cf16cc
Tomasz <tomaszrusin90@gmail.com>
2021-01-19 13:18:30 +0100

    change database

--- src/util/database.js
+++ src/util/database.js
@@ -1,14 +1,30 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("reservation", "root", "", {
-  host: "localhost",
-  dialect: "mariadb",
-  dialectOptions: {
-    timezone: "Etc/GMT+2",
-  },
-  define: {
-    freezeTableName: true,
-  },
-});
+const sequelize = new Sequelize(
+  "m1105_reservatio",
+  "m1105_reservatio",
+  "0Cl5uF8bTXkKnn4SiTt4",
+  {
+    host: "localhost",
+    dialect: "mariadb",
+    dialectOptions: {
+      timezone: "Etc/GMT+2",
+    },
+    define: {
+      freezeTableName: true,
+    },
+  }
+);
+
+// const sequelize = new Sequelize("reservation", "root", "", {
+//   host: "localhost",
+//   dialect: "mariadb",
+//   dialectOptions: {
+//     timezone: "Etc/GMT+2",
+//   },
+//   define: {
+//     freezeTableName: true,
+//   },
+// });
 
 module.exports = sequelize;
